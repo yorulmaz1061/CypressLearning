05944aea1b7e18b8a08e6f49913b8737e054bc8c
yorulmaz1061 <ozan.yorulmaz@gmail.com>
2023-05-21 21:42:55 +0300

    add:new features

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -18,10 +18,41 @@ describe('Context: Test Cases', ()=>{
 
       //find element with class attribute and create a list then seelect 3rd element
       //from the list
+      // it is wrapped html element to cypress element
       cy.get('.btn.btn-primary').then(($buttons)=>{
         cy.wrap($buttons).eq(2).click();// zero-one-two.. equal to -->two is the third element
+        // like list in JAVA, list.get(index)
         //assert the text
         cy.contains('Clicked on button three!').should('be.visible');
+
+
+      })
+      // you got all buttons with tagName; each method is creating me a loop
+      cy.get('button').each((item, index, list)=>{
+        // assert the lenght of the list, verify number of buttons
+        expect(list).to.have.length(6);
+        expect(item).to.have.attr("onclick");
+
+      })
+
+      //I will get all buttons like previous approach, get only the item then check for text of each item;
+      // if it is equal to Button 4, then click on it. 
+
+      cy.get('button').each((item)=>{
+
+        if(item.text()=="Button 4"){
+          cy.log(item.text()); //this commmand write the text at the test console
+          //item.click(); you cannot use cypress click function on jquery element
+          // so, we must wrap it
+
+          cy.wrap(item).click();
+          cy.contains('Clicked on button four!').should('be.visible');
+
+        }
+
+        //npx cypress run --headless -b chrome
+        // for running headless chrome
+
       })
 
 
